8b6ff04c1542237fdd7652f90b1ff5dbee039fbb
Alex Antufiev <12766383+AlexAntufiev@users.noreply.github.com>
2017-12-23 22:12:48 +0300

    Update MergingPeekingIncreasingIterator.java

--- src/seminar1/iterators/MergingPeekingIncreasingIterator.java
+++ src/seminar1/iterators/MergingPeekingIncreasingIterator.java
@@ -1,38 +1,60 @@
-package seminar1.iterators;
+package structures_and_algorythms.iterators;
 
 import java.util.Comparator;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import structures_and_algorythms.collections.ArrayPriorityQueue;
 
 /**
  * Итератор возвращающий последовательность из N возрастающих итераторов в порядке возрастания
  * first = 1,3,4,5,7
  * second = 0,2,4,6,8
  * result = 0,1,2,3,4,4,5,6,7,8
- *
+ * <p>
  * Time = O(n + k * log n),
- *  n — количество итераторов
- *  k — суммарное количество элементов
+ * n — количество итераторов
+ * k — суммарное количество элементов
  */
 public class MergingPeekingIncreasingIterator implements Iterator<Integer> {
 
-    private Comparator<PeekingIncreasingIterator> comparator = (p1, p2) -> p1.peek().compareTo(p2.peek());
+    private Comparator<PeekingIncreasingIterator> comparator = Comparator.comparing(PeekingIncreasingIterator::peek);
+    private ArrayPriorityQueue<PeekingIncreasingIterator> queue;
 
     public MergingPeekingIncreasingIterator(IPeekingIterator... peekingIterator) {
-        /* TODO: implement it */
-//        for (int i = 0; i < peekingIterator.length; i++) {
-//            peekingIterator[i].hasNext();
-//        }
+        queue = new ArrayPriorityQueue<>();
+        for (IPeekingIterator aPeekingIterator : peekingIterator) {
+            queue.add((PeekingIncreasingIterator) aPeekingIterator);
+        }
     }
 
     @Override
     public boolean hasNext() {
-        /* TODO: implement it */
-        return false;
+        return queue.size() > 0;
     }
 
     @Override
     public Integer next() {
-        /* TODO: implement it */
-        return null;
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        PeekingIncreasingIterator iterator = queue.extractMin();
+        Integer nextInteger = iterator.next();
+        if (iterator.hasNext()) {
+            iterator.peek();
+            queue.add(iterator);
+        }
+        return nextInteger;
     }
+
+    public static void main(String[] args) {
+        IPeekingIterator iterator1 = new PeekingIncreasingIterator(1, 10, 10);
+        IPeekingIterator iterator2 = new PeekingIncreasingIterator(10, 10, 10);
+        IPeekingIterator iterator3 = new PeekingIncreasingIterator(100, 10, 10);
+        MergingPeekingIncreasingIterator iterator = new MergingPeekingIncreasingIterator(iterator1, iterator2, iterator3);
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
 }
